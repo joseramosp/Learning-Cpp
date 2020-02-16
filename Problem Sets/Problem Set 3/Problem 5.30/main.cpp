3c03f719a37a7ebc60818a49ae8c65a43a8f9d34
Jose Ramos <jmrp.04@gmail.com>
2020-02-16 17:38:06 -0500

    Problem set 3, problem 4 is done (4/6).

--- Problem Sets/Problem Set 3/Problem 5.30/main.cpp
+++ Problem Sets/Problem Set 3/Problem 5.30/main.cpp
@@ -9,9 +9,49 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
+class DollarAmount {
+public:
+
+    //DollarAmount();
+
+    explicit DollarAmount(int64_t value) : amount{value} { }
+    explicit DollarAmount(int64_t bills, int64_t pennies) : amount{bills*100 + pennies} { }
+
+    void add(DollarAmount right){
+        amount += right.amount;
+    }
+
+    void subtract(DollarAmount right){
+        amount -= right.amount;
+    }
+
+    void addInterest(int rate, int divisor){
+        DollarAmount interest {
+                (amount * rate + divisor / 2) / divisor
+        };
+
+        add(interest);
+    }
+
+    std::string toString() const {
+        std::string dollars{std::to_string(amount/100)};
+        std::string cents{std::to_string(std::abs(amount % 100))};
+        return dollars + "." + (cents.size() == 1 ? "0" : "") + cents;
+    }
+
+private:
+    int64_t amount{0};
+};
+
 int main() {
+    DollarAmount myMoney(100, 54);
+    cout << myMoney.toString() << std::endl;
+    myMoney.add(DollarAmount(100));
+    cout << myMoney.toString() << std::endl;
+
 
 }
