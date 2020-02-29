ded8f0cd104e1c3293c8deb2e3f734a027c845d7
Jose Ramos <jmrp.04@gmail.com>
2020-02-29 18:50:19 -0500

    Starting problem set 5

---
+++ Problem Sets/Problem Set 5/cmake-build-debug/CMakeFiles/Problem_Set_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem_Set_5.dir/main.cpp.o"
+  "Problem_Set_5"
+  "Problem_Set_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem_Set_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
