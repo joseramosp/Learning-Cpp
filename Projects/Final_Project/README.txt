7003ff56745f7b8ebe21211ca692670bb4728bfe
Jose Ramos <jmrp.04@gmail.com>
2020-05-11 07:58:38 -0400

    Final Project: Cleaning up

---
+++ Projects/Final_Project/README.txt
@@ -0,0 +1,22 @@
+* Jose Ramos
+* Student ID: 00299444
+* C++ Programming Language - CIS 240
+* May 7 of 2020
+* Final Project
+---------------------------
+
+* Objective:
+
+This is a chance to show off anything you want. You can continue add to risk or you can make something totally new (COVID-19 infection simulator?). I just want to see what you can do with all we have covered.
+
+* NOTE: 
+
+I decided to do a COVID-19 Simulator.
+
+In order to run the graphics, make sure you have the working directory set as the project folder.
+
+The most difficult part was doing the UI and reading the documentation of the SFML. I learned that SFML doesn't have a button class, so I had to create my own.
+
+* Citations:
+
+I downloaded the SFML library from here -> https://www.sfml-dev.org/download/csfml/
---
+++ Projects/Final_Project/cmake-build-debug/CMakeFiles/Final_Project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jose/Library/Mobile Documents/com~apple~CloudDocs/NECC/Spring 2020/C++ Programming Language - CIS 240/Problem Sets and Projects /Projects/Final_Project/PersonNode.cpp" "/Users/jose/Library/Mobile Documents/com~apple~CloudDocs/NECC/Spring 2020/C++ Programming Language - CIS 240/Problem Sets and Projects /Projects/Final_Project/cmake-build-debug/CMakeFiles/Final_Project.dir/PersonNode.cpp.o"
+  "/Users/jose/Library/Mobile Documents/com~apple~CloudDocs/NECC/Spring 2020/C++ Programming Language - CIS 240/Problem Sets and Projects /Projects/Final_Project/main.cpp" "/Users/jose/Library/Mobile Documents/com~apple~CloudDocs/NECC/Spring 2020/C++ Programming Language - CIS 240/Problem Sets and Projects /Projects/Final_Project/cmake-build-debug/CMakeFiles/Final_Project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
